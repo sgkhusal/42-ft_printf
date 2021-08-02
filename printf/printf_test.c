9535a233ead020b88c35260359db389cc1833b54
user42 <sheela.khusal.08@gmail.com>
2021-08-02 19:00:30 +0200

    first commit

---
+++ printf/printf_test.c
@@ -0,0 +1,183 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_test.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguilher <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/03 22:46:27 by sguilher          #+#    #+#             */
+/*   Updated: 2021/07/07 23:09:26 by sguilher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_printf.h"
+
+int	main(void){
+	/* ************************* Printf ******************************** */
+	// int	printf(const char *format, ...)
+	// formatation tags: [flags][width][.precision][length]specifier
+	// return value: the total number of characters written. On failure, return a negative number.
+
+	printf(LIGHT_PURPLE("\nLet's see how printf function works?\n\n"));
+	printf(LIGHT_BLUE2("formatation tags: %%[flags][width][.precision][length]specifier\n\n"));
+	printf(LIGHT_PURPLE("************************************* Mandatory *************************************\n\n"));
+	printf(LIGHT_RED("Specifiers: c, s, d, i, u, p, x and X\n\n"));
+
+	char	c = 'a';
+	char	str[13] = "Hello World!";
+	int		i = -42;
+	unsigned int	j = 42;
+	int *ptr = &i;
+
+	// ***************** basic printing tests ************************ \\
+
+	// printing %
+	printf(PINK("printing %%:\n"));
+	printf(LIGHT_BLUE2("test %% without flags, width, precision or a specifier after - it prints just the %:\n"));
+	printf(LIGHT_BLUE2("%_this is a test\n"));
+	printf(LIGHT_BLUE2("%kanother test\n"));
+
+	printf(PINK("\nwhen we do not indicate the variable, it prints anything from a place in the memory according with the variable type:\n"));
+	printf(LIGHT_BLUE2("%%c: %c\n"));
+	printf(LIGHT_BLUE2("%%i: %i\n"));
+	printf(LIGHT_BLUE2("%%x: %x\n"));
+
+	// Flags:
+	// The flag '-' left-justifies within the given field width. Right justification is the default
+	// The flag 0 left-pads the number with zeroes instead of spaces, where padding is specified
+
+	// Width:
+	/* number - minimum number of charcters to be printed. If the value to be printed is shorter than this number,
+	the result is padded with blank spaces. The value is not truncated even if the result is larger */
+	/* '*' - the width is not specified in the format string, but as an additional integer value argument preceding
+	the argument that has to be formatted */
+
+	// Precision
+	// .number
+	/* When no precision is specified, the default is 1.
+	If the period is specified without an explicit value for precision, 0 is assumed */
+	/* For integer specifiers d, i, o, u, x and X specifies the minimum number of digits to be written.
+	If the value to be written is shorter than this number, the result is padded with leading zeros.
+	The value is not truncated even if the result is longer.
+	A precision of 0 means that no character is written for the value of 0 */
+	/* For e, E and f: is the number of digits to be printed after the decimal point */
+	/* For g an G - is the maximum number of significant digits to be printed */
+	/* For s is the maximum number of significant digits to be printed
+	(By default all characters are printed until the ending null character is encountered) */
+	/* For c it is has no effect */
+	// '.*' --> the precision is not specified in the format string, but as an additional integer value argument preceding the argument that has to be formatted
+
+	// test any combination of the flags '-0.*'
+
+	// printing char - specifier c
+	printf(LIGHT_RED("\n\nprinting char - specifier c:\n"));
+	printf(LIGHT_BLUE2("char %%c = %c\n"), c);
+	printf(LIGHT_BLUE2("char %%c < 0: %c\n"), -1);
+	printf(LIGHT_BLUE2("char %%c > 256: %c\n"), 257);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+	printf(LIGHT_BLUE2("%35s = |%c|\n"), "%%c", c);
+	printf(LIGHT_BLUE2("%35s = |%5c|\n"), "%%5c", c);
+	printf(LIGHT_BLUE2("%35s = |%-5c|\n"), "%%-5c", c);
+	printf(LIGHT_BLUE2("%35s = |%05c|\n"), "%%05c", c);
+	printf(LIGHT_BLUE2("%35s = |%-05c|\n"), "%%-05c", c);
+	printf(LIGHT_BLUE2("%35s = |%0-5c|\n"), "%%0-5c", c);
+
+	// printing string - specifier s
+	printf(LIGHT_RED("\n\nprinting string - specifier s:\n"));
+	printf(LIGHT_BLUE2("string %%s = %s\n"), str);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+	printf(LIGHT_BLUE2("%30s |%-010s|\n"), "%%-010s =", str);
+	printf(LIGHT_BLUE2("%30s |%-012s|\n"), "%%-012s =", str);
+	printf(LIGHT_BLUE2("%30s |%20s|\n"), "%%20s =", str);
+	printf(LIGHT_BLUE2("%30s |%-20s|\n"), "%%-20s =", str);
+	printf(LIGHT_BLUE2("%30s |%020s|\n"), "%%020s =", str);
+	printf(LIGHT_BLUE2("%30s |%20s|\n"), "%%20s =", str);
+	printf(LIGHT_BLUE2("%30s |%-020s|\n"), "%%-020s =", str);
+
+
+	// printing signed decimal integer - specifiers d and i
+	printf(LIGHT_RED("\n\nprinting signed decimal integer - specifiers d and i:\n"));
+	printf(LIGHT_BLUE2("number %%d = %d\n"), i);
+	printf(LIGHT_BLUE2("number %%d using pointer = %d\n"), *ptr);
+	printf(LIGHT_BLUE2("number %%d > maximum signed integer = %d\n"), 2147483648);
+	printf(LIGHT_BLUE2("number %%d < minimum signed integer = %d\n"), -2147483649);
+	printf(LIGHT_BLUE2("number %%i = %i\n"), i);
+	printf(LIGHT_BLUE2("number %%i using pointer = %i\n"), *ptr);
+	printf(LIGHT_BLUE2("number %%i > maximum signed integer = %i\n"), 2147483648);
+	printf(LIGHT_BLUE2("number %%i < minimum signed integer = %i\n"), -2147483649);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+	printf(LIGHT_BLUE2("%35s = |%5i|\n"), "%%5i", i);
+	printf(LIGHT_BLUE2("%35s = |%-5i|\n"), "%%05i", i);
+	printf(LIGHT_BLUE2("%35s = |%05i|\n"), "%%05i", i);
+	printf(LIGHT_BLUE2("%35s = |%-05i|\n"), "%%-05i", i);
+	printf(LIGHT_BLUE2("%35s = |%5i|\n"), "%%5i", 1000000);
+	printf(LIGHT_BLUE2("%35s = |%05i|\n"), "%%05i", 1000000);
+	printf(LIGHT_BLUE2("%35s = |%-05i|\n"), "%%-05i", 1000000);
+
+	// printing unsigned integer - specifier u:
+	printf(LIGHT_RED("\n\nprinting unsigned integer - specifier u:\n"));
+	printf(LIGHT_BLUE2("number %%u = %u\n"), j);
+	printf(LIGHT_BLUE2("number %%u negative = %u\n"), i);
+	printf(LIGHT_BLUE2("number %%u < 0 = %u\n"), -1);
+	printf(LIGHT_BLUE2("number %%u > maximum unsigned integer = %u\n"), 4294967296);
+	printf(LIGHT_BLUE2("number %%u > maximum unsigned integer = %u\n"), 4294967297);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+	printf(LIGHT_BLUE2("%%10u = |%10u|\n"), j);
+	printf(LIGHT_BLUE2("width with flag   %%010u = |%010u|\n"), j);
+	printf(LIGHT_BLUE2("width with flag   %%-010u = |%-010u|\n"), j);
+
+	// printing pointer - specifier p
+	printf(LIGHT_RED("\n\nprinting pointer - specifier p:\n"));
+	printf(LIGHT_BLUE2("char pointer %%p = %p\n"), &c);
+	printf(LIGHT_BLUE2("string pointer %%p = %p\n"), &str[0]);
+	printf(LIGHT_BLUE2("int pointer %%p = %p\n"), ptr);
+	printf(LIGHT_BLUE2("unsigned int pointer %%p = %p\n"), &j);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+
+	// printing unsigned hex integer lowercase - specifier x
+	printf(LIGHT_RED("\n\nprinting unsigned hex integer lowercase - specifier x:\n"));
+	printf(LIGHT_BLUE2("unsigned hex integer %%x = %x\n"), j);
+	printf(LIGHT_BLUE2("unsigned hex integer with negative number %%x = %x\n"), i);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+
+	// printing unsigned hex integer uppercase - specifier X
+	printf(LIGHT_RED("\n\nprinting unsigned hex integer uppercase - specifier X:\n"));
+	printf(LIGHT_BLUE2("unsigned hex integer %%X = %X\n"), j);
+	printf(LIGHT_BLUE2("unsigned hex integer with negative number %%X = %X\n"), i);
+	printf(PINK("\n------------------------------- Testing Flags ----------------------------\n\n"));
+
+
+	/* ************************ Bonus ****************************** */
+	printf(LIGHT_PURPLE("\n************************ BONUS ******************************\n\n"));
+	// Specifiers
+	// specifier n - nothing printed
+	// printing decimal floating point - specifier f
+	// printing scientific notation using 'e' character - specifier e
+	// printing shorter of %e or %f - specifier g
+
+	// Flags
+	// flag '+' --> forces to precede the result with a plus or minus sign, even for positive numbers
+	// flag (space) --> if no sign is going to be written, a blank space is inserted before the value
+	// flag '#' --> used with o, x or X specifiers - the value is preceded with 0, 0x or 0X for values different than zero
+	// flag '#' --> used with e, E and f specifiers - it forces the written output to contain a decimal point even if no digit would follow
+	// flag '#' --> used with g, G - the result is the same as with e or E, but trailing zeros are removed
+
+	// Length
+	// h - the argument is interpreted as a short int or unsigned short int (only for i, d, o, u, x, X)
+	// hh - ??
+	// l - the argument is interpreted as a long int or unsigned long int (only for i, d, o, u, x, X)
+	// and as a wide character or wide character string (c and s)
+	// ll - ??
+
+
+	/* ************************ Plus ****************************** */
+	// Specifiers
+	// printing signed octal - specifier o
+	// printing scientific notation using 'E' character - specifier E
+	// printing shorter of %E or %f - specifier G
+
+	// Length
+	// L - the argument is interpreted as a long double (only for e, E, f, g, G)
+
+	return (0);
+}
---
+++ printf/test_printf.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_printf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguilher <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 19:53:27 by sguilher          #+#    #+#             */
+/*   Updated: 2021/07/07 19:56:18 by sguilher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_PRINTF_H
+# define TEST_PRINTF_H
+
+# include "colors.h"
+# include <stdio.h>
+
+# define PURPLE(string)			"\033[38;5;200m" string "\033[0m"
+# define PURPLE2(string)		"\033[0;35m" string "\033[0m"
+# define LIGHT_PURPLE(string)	"\033[1;35m" string "\033[0m"
+# define PINK(string)			"\033[38;5;212m" string "\033[0m"
+# define LIGHT_RED(string)		"\033[1;31m" string "\033[0m"
+# define RED(string)			"\033[38;5;196m" string "\033[0m"
+# define GREEN(string)			"\033[0;32m" string "\033[0m"
+# define GREEN2(string)			"\033[38;5;83m" string "\033[0m"
+# define GREEN_BOLD(string)		"\033[1;32m" string "\033[0m"
+# define GREEN3(string)		"\033[38;5;43m" string "\033[0m"
+# define BLUE(string)			"\033[0;34m" string "\033[0m"
+# define LIGHT_BLUE(string)		"\033[1;34m" string "\033[0m"
+# define LIGHT_BLUE2(string)			"\033[38;5;75m" string "\033[0m"
+# define CYAN(string)			"\033[0;36m" string "\033[0m"
+# define LIGHT_CYAN(string)		"\033[1;36m" string "\033[0m"
+# define YELLOW(string)			"\033[1;33m" string "\033[0m"
+# define BROWN(string)			"\033[0;33m" string "\033[0m"
+# define BLACK(string)			"\033[0;30m" string "\033[0m"
+# define DARK_GREY(string)			"\033[38;5;240m" string "\033[0m"
+# define GREY(string)			"\033[38;5;244m" string "\033[0m"
+# define LIGHT_GREY(string)		"\033[0;37m" string "\033[0m"
+# define WHITE(string)			"\033[1;37m" string "\033[0m"
+# define RESET					"\033[0m"
+
+# define NOT_OK	0
+# define OK		1
+
+#endif
