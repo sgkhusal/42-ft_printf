8162c96dcd4b85558c88229b77c1d7f9b7c43048
user42 <sheela.khusal.08@gmail.com>
2021-08-18 23:08:28 +0200

    tests guacamole

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: sguilher <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 17:40:46 by sguilher          #+#    #+#             */
-/*   Updated: 2021/08/18 22:57:28 by sguilher         ###   ########.fr       */
+/*   Updated: 2021/08/18 23:07:44 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -228,7 +228,7 @@ void	upperhex_test()
 	check_return(result1, result2);
 }
 
-/*void	_test()
+/*void	p_test()
 {
 	int	result1;
 	int	result2;
@@ -240,18 +240,19 @@ int	main(void){
 
 	printf(LIGHT_PURPLE("\nFT_PRINTF TESTER\n\n"));
 
-	//text_tests();
+	text_tests();
 
 	printf(LIGHT_RED("Specifiers: c, s, d, i, u, p, and x\n"));
 
-	//percentage_test();
-	//empty_var_test();
-	//c_test();
-	//s_test();
-	//di_test();
-	//u_test();
+	percentage_test();
+	empty_var_test();
+	c_test();
+	s_test();
+	di_test();
+	u_test();
 	lowerhex_test();
 	upperhex_test();
+	//p_test();
 
 	//int *ptr = &i;
 	// printing pointer - specifier p
