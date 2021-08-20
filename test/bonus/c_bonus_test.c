9473b0d502de9d73235d9cb9af23c1d3db8f710c
user42 <sheela.khusal.08@gmail.com>
2021-08-20 06:17:56 +0200

    added bonus tests for c = \0

--- test/bonus/c_bonus_test.c
+++ test/bonus/c_bonus_test.c
@@ -6,7 +6,7 @@
 /*   By: sguilher <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 04:13:21 by sguilher          #+#    #+#             */
-/*   Updated: 2021/08/20 05:42:02 by sguilher         ###   ########.fr       */
+/*   Updated: 2021/08/20 06:16:38 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,13 @@ void	c_bonus_test()
 	result2 = ft_printf("%%5c = |%5c|\n", c);
 	check_return(result1, result2);
 
+	printf("---------------\n");
 	printf(LIGHT_BLUE2("test 2: left padding\n"));
 	result1 = printf("%%-5c = |%-5c|\n", c);
 	result2 = ft_printf("%%-5c = |%-5c|\n", c);
 	check_return(result1, result2);
 
+	printf("---------------\n");
 	printf(LIGHT_BLUE2("test 3: flags\n"));
 	result1 = printf("%%05c = |%05c|\n", c); // undefined behavior
 	result2 = ft_printf("%%05c = |%05c|\n", c);
@@ -46,6 +48,7 @@ void	c_bonus_test()
 	result1 = printf("%%#5c = |%#5c|\n", c); // undefined behavior
 	result2 = ft_printf("%%#5c = |%#5c|\n", c);
 	check_return(result1, result2);
+	printf("---------------\n");
 	result1 = printf("%%5.c = |%5.c|\n", c);
 	result2 = ft_printf("%%5.c = |%5.c|\n", c);
 	check_return(result1, result2);
@@ -53,6 +56,7 @@ void	c_bonus_test()
 	result2 = ft_printf("%%.5c = |%.5c|\n", c);
 	check_return(result1, result2);
 
+	printf("---------------\n");
 	printf(LIGHT_BLUE2("test 4:\n"));
 	result1 = printf("%%5-c = |%5-c|\n", c); // - vem sempre antes do width
 	result2 = ft_printf("%%5-c = |%5-c|\n", c);
@@ -67,6 +71,7 @@ void	c_bonus_test()
 	result2 = ft_printf("%%5#c = |%5#c|\n", c);
 	check_return(result1, result2);
 
+	printf("---------------\n");
 	printf(LIGHT_BLUE2("test 5:\n"));
 	result1 = printf("%%-010.3c = |%-010.3c|\n", c); // # vem sempre antes do width
 	result2 = ft_printf("%%-010.3c = |%-010.3c|\n", c);
@@ -79,5 +84,20 @@ void	c_bonus_test()
 	check_return(result1, result2);
 	result1 = printf("%%-0.5c = |%-0.5c|\n", c); // undefined behavior
 	result2 = ft_printf("%%-0.5c = |%-0.5c|\n", c);
+	check_return(result1, result2);
+
+	printf("---------------\n");
+	printf(LIGHT_BLUE2("\ntest 6: \\0\n"));
+	result1 = printf("%%c = |%c|\n", '\0');
+	result2 = ft_printf("%%c = |%c|\n", '\0');
+	check_return(result1, result2);
+	result1 = printf("%%5c = |%5c|\n", '\0');
+	result2 = ft_printf("%%5c = |%5c|\n", '\0');
+	check_return(result1, result2);
+	result1 = printf("%%-5c = |%-5c|\n", '\0');
+	result2 = ft_printf("%%-5c = |%-5c|\n", '\0');
+	check_return(result1, result2);
+	result1 = printf("%%.5c = |%.5c|\n", '\0');
+	result2 = ft_printf("%%.5c = |%.5c|\n", '\0');
 	check_return(result1, result2);
 }
